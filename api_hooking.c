f4b0b811046f72f6becdf6572c2d1fbf0a68fc7a
tolerans <scriptkidceo@gmail.com>
2020-06-12 23:40:06 +0300

    Update

--- api_hooking.c
+++ api_hooking.c
@@ -1,6 +1,10 @@
 #include <Windows.h>
 
+typedef int (WINAPI *pMessageBoxW)(HWND, LPCWSTR, LPCWSTR, UINT);
+int WINAPI MyMessageBoxW(HWND, LPCWSTR, LPCWSTR, UINT);
+
 int main (void) {
 
+    printf("Debug\n");
     return EXIT_SUCCESS;
 }
